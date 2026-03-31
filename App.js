cb0478a7dd427b5489a179f5d420f3cc53a6eb0d
sanjayloganathan777 <l3674437@gmail.com>
2026-03-31 17:17:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BookingForm from "./components/BookingForm";
+import "./styles.css";
+
+function App() {
+  return (
+    <main>
+      <h1>Little Lemon Table Booking</h1>
+      <BookingForm />
+    </main>
+  );
+}
+
+export default App;
